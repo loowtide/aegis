a6d8c6727927784b8a5d6e6e0830bba34f05fe72
loowtide <200423340+loowtide@users.noreply.github.com>
2026-02-18 02:21:06 +0530

    feat: add RateLimit model

--- aegis/apps.py
+++ aegis/apps.py
@@ -1,9 +1,12 @@
 from django.apps import AppConfig
-
+from typing import Any
 
 class AegisConfig(AppConfig):
-    default_auto_field="django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.BigAutoField"
     name = "aegis"
-    defaults={
-        "cooldown":7
+    defaults = {"cooldown": 10}
+    RateLimitDefaults: dict[str, Any]={
+    'CAPACITY': 10.0,
+    'REFILL_RATE': 1.0,
+    'EXPIRATION_DAYS': 7,
     }
--- aegis/models.py
+++ aegis/models.py
@@ -1,12 +1,10 @@
-import datetime
-from datetime import timezone
-
 from django.db import models
 from django.db.models import (
     DateTimeField,
     GenericIPAddressField,
-    IntegerField,
+    PositiveIntegerField,
     TextField,
+    FloatField,
 )
 from django.utils import timezone as utils_timezone
 from .apps import AegisConfig
@@ -17,9 +15,11 @@ class BlockedIP(models.Model):
     )
     reason = TextField()
     datetime_added = DateTimeField(default=utils_timezone.now, db_index=True)
-    cooldown = IntegerField(default=AegisConfig.defaults["cooldown"],help_text="Cooldown period")
+    cooldown = PositiveIntegerField(
+        default=AegisConfig.defaults["cooldown"], help_text="Cooldown period"
+    )
     last_seen = DateTimeField(blank=True, null=True, db_index=True)
-    tally = IntegerField(default=0, help_text="No of times this ip has been blocked")
+    tally = PositiveIntegerField(default=0, help_text="No of times this ip has been blocked")
 
     class Meta:
         get_latest_by = "datetime_added"
@@ -31,8 +31,16 @@ class BlockedIP(models.Model):
         return f"{self.ip}"
 
     def has_expired(self):
-        quiet_time = datetime.datetime.now(timezone.utc)-(self.last_seen or self.datetime_added)
-        return quiet_time.days >= self.cooldown
+        quiet_time = utils_timezone.now() - (self.last_seen or self.datetime_added)
+        return quiet_time.total_seconds() >= self.cooldown
 
 class RateLimit(models.Model):
-   pass
+    ip=GenericIPAddressField(db_index=True,verbose_name="IP address",primary_key=True)
+    bucket_level=FloatField(default=10.0)
+    last_updated=DateTimeField(auto_now=True)
+    max_capacity=FloatField(default=AegisConfig.RateLimitDefaults["CAPACITY"])
+    refill_rate=FloatField(default=AegisConfig.RateLimitDefaults["REFILL_RATE"])
+    expiration_days=PositiveIntegerField(default=AegisConfig.RateLimitDefaults["EXPIRATION_DAYS"])
+
+    def __str__(self) -> str:
+        return f"{self.ip} - {self.bucket_level}/{self.max_capacity}"
